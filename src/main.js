5d1a3364d8854079fd7e4966fc1ddb3a8106e923
pzyu <pangzhengyu@live.com>
2017-01-21 19:40:33 +0800

    WPM layer

--- src/main.js
+++ src/main.js
@@ -20,12 +20,13 @@ Base.Main.prototype = {
 		game.load.image("spark", "assets/spark.png");
 		// Load sprite sheet (key, path, width, height, numOfFrames)
 		//game.load.spritesheet("trump", "assets/spritesheet.png", 64, 64, 30);
+		game.load.json("test", "assets/spritesheet.json");
 		
 	},
 
 	create: function() {
 
-
+		console.log(game.cache.getJSON("test"));
 
 		// Add background
 		var background = game.add.sprite(0, 0, "background");
@@ -34,16 +35,12 @@ Base.Main.prototype = {
 
 		// Add emitter
 		this.emitter = game.add.emitter(0, 0, 100);
-		console.log(this.emitter);
-
 		// Init emitter
 		this.emitter.makeParticles("spark");
 		this.emitter.gravity = 200;
 		this.emitter.setAlpha(0, 1, 100);
 
-		// Add WPM
-
-				// Add timer
+		// Add timer
 		var text = 0;
 		var timeElapsed = 0;
 		
@@ -53,9 +50,6 @@ Base.Main.prototype = {
 			var WPM = Number(Base.correctChars / 5 / (timeElapsed / 60)).toFixed(0);
 			text.setText("WPM:" + WPM);
 		}
-
-		text = game.add.text(100, 75, "WPM:0", { font: "32px myfont", fill: "#ffffff", align: "left" });
-    
     	text.anchor.setTo(0, 0);
 
     	game.time.events.loop(Phaser.Timer.SECOND, logTime, this);
@@ -67,6 +61,8 @@ Base.Main.prototype = {
 			Base.tweetList[i] = newTweet;
 			game.add.existing(newTweet);
 		}
+
+		text = game.add.text(100, 75, "WPM:0", { font: "32px myfont", fill: "#ffffff", align: "left" });
 
 	    game.physics.startSystem(Phaser.Physics.ARCADE);
 
